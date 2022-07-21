23ff3e28b3bcd8fdc23a75c2a3722c8b3c8e835f
ScriptingOtaku <0822djs@gmail.com>
2022-07-21 16:37:07 -0700

    spawned bircks to do quests

--- src/server/blahaj.server.ts
+++ src/server/blahaj.server.ts
@@ -2,10 +2,9 @@
 // On baseplate clicked quest gets updated.
 
 import { Workspace } from "@rbxts/services";
-import Remotes from "shared/remotes";
 import { QuestService } from "./services/QuestService";
 import { PlayerQuestAction } from "./signals";
-const baseplate = Workspace.WaitForChild("Baseplate");
+/**const baseplate = Workspace.WaitForChild("Baseplate");
 const spawn = Workspace.WaitForChild("SpawnLocation");
 
 const questUpdate = new Instance("ClickDetector", spawn);
@@ -13,3 +12,18 @@ questUpdate.MouseClick.Connect((player: Player) => PlayerQuestAction.Fire(player
 
 const questAdd = new Instance("ClickDetector", baseplate);
 questAdd.MouseClick.Connect((player: Player) => QuestService.AddQuest(player, math.random(1, 2)));
+*/
+
+const AddQuest = new Instance("Part", Workspace);
+AddQuest.Anchored = true;
+AddQuest.BrickColor = BrickColor.Green();
+AddQuest.Position = new Vector3(2, 2, -5);
+const AddQuestClicker = new Instance("ClickDetector", AddQuest);
+AddQuestClicker.MouseClick.Connect((player: Player) => QuestService.AddQuest(player, math.random(1, 2)));
+
+const UpdateQuest = new Instance("Part", Workspace);
+UpdateQuest.Anchored = true;
+UpdateQuest.BrickColor = BrickColor.Red();
+UpdateQuest.Position = new Vector3(-2, 2, -5);
+const UpdateQuestClicker = new Instance("ClickDetector", UpdateQuest);
+UpdateQuestClicker.MouseClick.Connect((player: Player) => PlayerQuestAction.Fire(player, 0));
